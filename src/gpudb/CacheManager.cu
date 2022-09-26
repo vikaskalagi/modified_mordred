a1c9f5f45e66cb0a659dfcdcf1b077f1b82f7b4e
vikaskalagi <vikaskalagi123@gmail.com>
2022-09-26 10:54:21 +0530

    test

--- src/gpudb/CacheManager.cu
+++ src/gpudb/CacheManager.cu
@@ -1296,7 +1296,7 @@ CacheManager::loadColumnToCPU() {
 	h_lo_quantity[1] = 22;
 	h_lo_quantity[2] = 30;
 	h_lo_quantity[3] = 23;
-	h_lo_quantity[4] = 224;
+	h_lo_quantity[4] = 24;
 
 	h_lo_extendedprice[0] = 1;
 	h_lo_extendedprice[1] = 2;
